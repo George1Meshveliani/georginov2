c3c76350f6bad3e4d6562bf5fdab1b5aa46835ca
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-05-23 02:47:10 +0400

    Main page.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import MainPage from './Pages/MainPage';
+import './App.css';
+
+function App() {
+  return (
+<div>
+  <MainPage />
+</div>
+    // <div className="App">
+    //   {/* <header className="App-header">
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+    // </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/MainPage.js
@@ -0,0 +1,83 @@
+
+
+const MainPage = () => {
+    return (
+        <div class="content">
+        <figure class="text-center">
+            <blockquote class="blockquote">
+                <p>Georgian traditional high-quality homemade wine.</p>
+            </blockquote>
+            <figcaption class="blockquote-footer">
+                <cite title="Source Title">Recognized by world</cite>
+            </figcaption>
+        </figure>
+    
+        <p>
+            The roots of Georgian
+            viticulture have been traced back by archeology to when people of the
+            South Caucasus discovered that wild grape juice turned into wine when
+            it was left buried through the winter in a shallow pit. This knowledge
+            was nourished by experience, and from 6000 BC inhabitants of the current
+            Georgia were cultivating grapes and burying clay vessels, kvevris, in which
+            to store their wine ready for serving at ground temperature. When filled with
+            the fermented juice of the harvest, the kvevris are topped with a wooden
+            lid and then covered and sealed with earth. Some may remain entombed for up to 50 years.
+            Due to its diverse and unique microclimate, there are about 500 grape varieties in modern Georgia.
+        </p>
+        <p>
+    
+        </p>
+        <p>
+    
+        </p>
+        <span id="imgC"></span>
+        <p>
+    
+        </p>
+        <p>
+            Wine vessels of every shape, size and design
+            have been the crucial part of pottery in Georgia for millennia.
+            Ancient artifacts attest to the high skill of local craftsmen. Among vessels, the most ubiquitous and unique
+            to Georgian wine-making culture are probably the Kvevris,
+            very large earthenware vessels with an inside coat of beeswax. Not only kvevris were used to
+            ferment grape juice and to store up wine, but also chapi and satskhao; others yet
+            were used for drinking, such as khelada, doki, sura, chinchila, deda-khelada, dzhami and marani.
+        </p>
+    
+        <p>
+    
+            The continuous importance of winemaking and drinking in Georgian culture is also visible in
+            various antique works of art. Many of the unearthed silver, gold and bronze artifacts of the
+            3rd and 2nd millennia BC bear chased imprints of the vine, grape clusters and leaves.
+            The State Museum of Georgia has on display a cup of high-carat gold set with gems,
+            an ornamented silver pitcher and some other artifacts dated to the 2nd millennium BC.
+            From classical Antiquity, Georgian museums display a cameo depicting Bacchus,
+            and numerous sarcophagi with wine pitchers and ornamented wine cups found in ancient tombs.
+        </p>
+        <div id="imgB"></div>
+        <p>
+    
+        </p>
+        <p>
+            Traditional Georgian grape varieties are little known in the World.
+            Now that the wines of Eastern and Central Europe are coming to international
+            awareness, grapes from this region are becoming better known. Although there are nearly 400 to
+            choose from, only 38 varieties are officially grown for commercial viticulture in Georgia
+        </p>
+    
+        <p>
+    
+            Georgia's territorial and climate conditions are optimal for wine-making.
+            Extremes of weather are unusual: summers tend to be short-sleeve sunny, and
+            winters mild and frost-free. Natural springs abound, and the Caucasian Mountain
+            streams drain mineral-rich water into the valleys. Georgia's moderate climate and
+            moist air, influenced by the Black Sea, provide the best conditions for vine cultivating.
+            The soil in vineyards is so intensively cultivated that the grapevines grow up the trunks
+            of fruit trees eventually hanging down along the fruit when they ripen. This method of
+            cultivation is called maglari.
+    
+        </p>
+        </div>
+    )
+}
+export default MainPage;
