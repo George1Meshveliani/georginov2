965bdb4bff166e76cac93083a51325f774f0e636
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-05-23 06:06:43 +0400

    Make corrections.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import MainPage from './Pages/MainPage';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Footer from './Pages/Footer';
-// import Header from './Pages/Header';
 import Founder from './Pages/Founder';
 import Contact from './Pages/Contact';
 
@@ -33,23 +32,7 @@ function App() {
                          <Footer /> 
                     </Router>
                 </header>
-  {/* {/* <MainPage /> */}
 </div>
-    // <div className="App">
-    //   {/* <header className="App-header">
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header> */}
-    // </div>
   );
 }
 
